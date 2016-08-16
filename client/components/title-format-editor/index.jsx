10c208570c4ec72f6548a95a8819cebcff0585f7
Dennis Snell <dennis.snell@automattic.com>
2016-08-16 13:35:46 -0700

    Create ' ' with entities on button clicks

--- client/components/title-format-editor/index.jsx
+++ client/components/title-format-editor/index.jsx
@@ -24,43 +24,7 @@ const editorStyle = {
 	borderRadius: '3px'
 };
 
-const getWordAt = ( string, position ) => {
-	// Perform type conversions.
-	const str = String( string );
-	const pos = Number( position ) >>> 0;
-
-	// Search for the word's beginning and end.
-	const left = str.slice( 0, pos + 1 ).search( /\S+$/ );
-	const right = str.slice( pos ).search( /\s/ );
-
-	// The last word in the string is a special case.
-	if ( right < 0 ) {
-		return {
-			word: str.slice( left ),
-			begin: left,
-			end: str.length,
-		};
-	}
-
-	// Return the word, using the located bounds to extract it from the string.
-	return {
-		word: str.slice( left, right + pos ),
-		begin: left,
-		end: right + pos,
-	};
-};
-
-const getSearchText = ( editorState, selection ) => {
-	const anchorKey = selection.getAnchorKey();
-	const anchorOffset = selection.getAnchorOffset() - 1;
-	const currentContent = editorState.getCurrentContent();
-	const currentBlock = currentContent.getBlockForKey( anchorKey );
-	const blockText = currentBlock.getText();
-	return getWordAt( blockText, anchorOffset );
-};
-
 const Token = props => {
-	console.log( props );
 	const { name } = Entity.get( props.entityKey ).data;
 
 	const style = {
@@ -80,15 +44,11 @@ export class TitleFormatEditor extends Component {
 		super( props );
 
 		this.state = {
-			editorState: EditorState.createEmpty( new CompositeDecorator( [ {
-				strategy: this.tokenStrategy,
-				component: Token
-			} ] ) )
+			editorState: EditorState.createEmpty()
 		};
 
 		this.onChange = this.onChange.bind( this );
 		this.addToken = this.addToken.bind( this );
-		this.tokenStrategy = this.tokenStrategy.bind( this );
 	}
 
 	onChange( editorState ) {
@@ -104,58 +64,24 @@ export class TitleFormatEditor extends Component {
 	addToken( name ) {
 		return () => {
 			const { editorState } = this.state;
-			const currentSelectionState = editorState.getSelection();
-			const { begin, end } = getSearchText( editorState, currentSelectionState );
+			const currentSelection = editorState.getSelection();
 
-			// Get the selection of the :emoji: search text
-			const emojiTextSelection = currentSelectionState.merge( {
-				anchorOffset: begin,
-				focusOffset: end,
-			} );
+			const tokenEntity = Entity.create( 'TOKEN', 'IMMUTABLE', { name } );
 
-			const entityKey = Entity.create( 'token', 'IMMUTABLE', { name } );
-
-			let emojiReplacedContent = Modifier.replaceText(
+			const contentState = Modifier.replaceText(
 				editorState.getCurrentContent(),
-				emojiTextSelection,
-				'',
+				currentSelection,
+				' ',
 				null,
-				entityKey
+				tokenEntity
 			);
 
-			// If the emoji is inserted at the end, a space is appended right after for
-			// a smooth writing experience.
-			const blockKey = emojiTextSelection.getAnchorKey();
-			const blockSize = editorState.getCurrentContent().getBlockForKey(blockKey).getLength();
-			if ( blockSize === end ) {
-				emojiReplacedContent = Modifier.insertText(
-					emojiReplacedContent,
-					emojiReplacedContent.getSelectionAfter(),
-					'',
-				);
-			}
-
-			const newEditorState = EditorState.push(
+			this.onChange( EditorState.push(
 				editorState,
-				emojiReplacedContent,
-				'insert-token',
-			);
-			return this.onChange( EditorState.forceSelection(
-				newEditorState,
-				emojiReplacedContent.getSelectionAfter()
+				contentState,
+				'replace-characters'
 			) );
 		};
-	}
-
-	tokenStrategy( contentBlock, callback ) {
-		contentBlock.findEntityRanges(
-			charMeta => {
-				const entity = charMeta.getEntity();
-
-				return entity && 'token' === Entity.get( entity ).type;
-			},
-			callback
-		);
 	}
 
 	render() {
